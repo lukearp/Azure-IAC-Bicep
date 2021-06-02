7a47a0e83f34ee64e567dfe53b0c1205ff39622c
Luke Arp <ruarp@microsoft.com>
2021-06-02 13:24:05 -0400

    Restart Script

--- Scripts/Restart-Vms/restart-vms.ps1
+++ Scripts/Restart-Vms/restart-vms.ps1
@@ -1,14 +1,14 @@
 Connect-AzAccount -Identity -SubscriptionId $args[2];
 $vms = $args[0].Split("[").Split("]").Split("'");
 foreach ($vm in $vms) {
-    if($vm -ne $null)
+    if($vm -ne "")
     {
         Stop-AzVM -Name $vm -ResourceGroupName $args[1] -Force -Confirm:$false;
     }
 }
 sleep 15;
 foreach ($vm in $vms) {
-    if($vm -ne $null)
+    if($vm -ne "")
     {
         Start-AzVM -Name $vm -ResourceGroupName $args[1];
     }
