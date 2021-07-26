a744c9de3d24358609e7f8d1a527217b2429e648
Luke Arp <ruarp@microsoft.com>
2021-07-26 09:11:50 -0400

    Remove Peering Deployment Script

---
+++ Scripts/Remove-VNET-Peers/Remove-VNET-Peers-DeploymentScript.ps1
@@ -0,0 +1,5 @@
+Connect-AzAccount -Identity -SubscriptionId $args[0].Split("/")[2];
+$peerRemove = Remove-AzVirtualNetworkPeering -VirtualNetworkName $args[0].Split("/")[8] -ResourceGroupName $args[0].Split("/")[4] -Name $args[0].Split("/")[10] -Force -Confirm:$false
+
+$DeploymentScriptOutputs = @{};
+$DeploymentScriptOutputs['output'] = $peerRemove
