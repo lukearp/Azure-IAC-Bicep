4d6d059a8aaf273996b6c18ea693ab951d9e3dc2
Luke Arp <ruarp@microsoft.com>
2021-08-16 17:23:48 -0400

    removed sleep

--- Scripts/Remove-VNET-Peers/Remove-VNET-Peers-DeploymentScript.ps1
+++ Scripts/Remove-VNET-Peers/Remove-VNET-Peers-DeploymentScript.ps1
@@ -1,5 +1,4 @@
 Connect-AzAccount -Identity -SubscriptionId $args[0].Split("/")[2];
 $peerRemove = Remove-AzVirtualNetworkPeering -VirtualNetworkName $args[0].Split("/")[8] -ResourceGroupName $args[0].Split("/")[4] -Name $args[0].Split("/")[10] -Force -Confirm:$false
-sleep 60
 $DeploymentScriptOutputs = @{};
 $DeploymentScriptOutputs['output'] = $peerRemove
