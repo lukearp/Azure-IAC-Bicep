81badb0b027dc0daa42870401b711f4c1c45256e
Luke Arp <ruarp@microsoft.com>
2021-08-26 09:43:26 -0400

    Testing Subnet Deployment Script

---
+++ Scripts/VNET-IP-Segmentation/VNET-AddressSpace-Carve-DeploymentScript.ps1
@@ -0,0 +1,45 @@
+param (
+    $subscription,
+    $subnets,
+    $vnetAddressSpaces
+)
+
+#Check for PS Subnet Carver Module
+try {
+    Import-Module -Name PSSubnetCarver
+} 
+catch {
+    Install-Module -Name PSSubnetCarver
+    Import-Module -Name PSSubnetCarver
+}
+for ($i = 0; $i -lt $vnetAddressSpaces.count; $i++)
+{
+    New-SCContext -Name $($subscription + "-" + $i) -RootAddressSpace $vnetAddressSpaces[$i]
+}
+
+foreach ($subnet in $subnets) {
+    if($subnet.cider -eq $true) {
+        for ($i = 0; $i -lt $vnetAddressSpaces.count; $i++) {
+            try {
+                $subnetReserve = Get-SCSubnet -Context $($subscription + "-" + $i) -ReserveCIDR $subnet.addressSize -ErrorAction SilentlyContinue
+                $subnet | Add-Member -Name AddressSpaceReserved -Value $($subnetReserve.NetworkIPAddress.ToString() + "/" + $subnetReserve.CIDR) -MemberType NoteProperty
+                break;
+            } catch {
+                
+            }            
+        }        
+    }
+    else {
+        for ($i = 0; $i -lt $vnetAddressSpaces.count; $i++) {
+            try {
+                $subnetReserve = Get-SCSubnet -Context $($subscription + "-" + $i) -ReserveCount $subnet.addressSize -ErrorAction SilentlyContinue
+                $subnet | Add-Member -Name AddressSpaceReserved -Value $($subnetReserve.NetworkIPAddress.ToString() + "/" + $subnetReserve.CIDR) -MemberType NoteProperty
+                break;
+            } catch {
+                
+            }            
+        }
+    }
+}
+$DeploymentScriptOutputs = @{};
+$DeploymentScriptOutputs['output'] = $subnets
