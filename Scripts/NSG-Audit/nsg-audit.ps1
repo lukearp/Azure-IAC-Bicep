b400c52a3bdf51d7399b4f53fc113a061b67e8e3
Luke Arp <ruarp@microsoft.com>
2022-04-12 18:33:07 -0400

    nsg audit modifications

--- Scripts/NSG-Audit/nsg-audit.ps1
+++ Scripts/NSG-Audit/nsg-audit.ps1
@@ -3,64 +3,60 @@ param (
 )
 
 $subscriptions = Get-AzSubscription -TenantId $tenantId
-$nsgInRules = @()
-$nsgBase = @()
-$connectedDevices = @()
-$vnetSubnetsWithoutNSGS = @()
 $report = @()
-foreach($sub in $subscriptions)
-{
-    Select-AzSubscription -SubscriptionObject $sub
+foreach ($sub in $subscriptions) {
+    $nsgInRules = @()
+    $nsgBase = @()
+    $connectedDevices = @()
+    $vnetSubnetsWithoutNSGS = @()
+    Select-AzSubscription -SubscriptionId $sub.SubscriptionId -TenantId $tenantId
+    $vnets = $null
+    $networkSecurityGroups = $null
     $vnets = Get-AzVirtualNetwork
     $networkSecurityGroups = Get-AzNetworkSecurityGroup
-    foreach($nsg in $networkSecurityGroups)
-    {
-        if($nsg.SecurityRules.Count -gt 0 -and ($nsg.NetworkInterfaces.Count -gt 0 -or $nsg.Subnets.Count -gt 0))
-        {
+    foreach ($nsg in $networkSecurityGroups) {
+        if ($nsg.SecurityRules.Count -gt 0 -and ($nsg.NetworkInterfaces.Count -gt 0 -or $nsg.Subnets.Count -gt 0)) {
             $nsgInRules += New-Object -TypeName psobject -Property @{
                 nsgResourceGroup = $nsg.Id.Split("/")[4]
-                nsgName = $nsg.Name
-                rules = $nsg.SecurityRules | ?{$_.Direction -eq "Inbound"}
-                subnets = $nsg.Subnets.Count -gt 0 ? $nsg.Subnets.Id.Split("/")[-1] : @()
-                interfaces = $nsg.NetworkInterfaces.Count -gt 0 ? $nsg.NetworkInterfaces.Id : @()
+                nsgName          = $nsg.Name
+                rules            = $nsg.SecurityRules | ? { $_.Direction -eq "Inbound" }
+                subnets          = $nsg.Subnets.Count -gt 0 ? $nsg.Subnets.Id : @()
+                interfaces       = $nsg.NetworkInterfaces.Count -gt 0 ? $nsg.NetworkInterfaces.Id : @()
             }
         }
         else {
-            $nsgBase +=  New-Object -TypeName psobject -Property @{
+            $nsgBase += New-Object -TypeName psobject -Property @{
                 nsgResourceGroup = $nsg.Id.Split("/")[4]
-                nsgName = $nsg.Name
-                rules = $nsg.DefaultSecurityRules | ?{$_.Direction -eq "Inbound"}
-                subnets = $nsg.Subnets.Count -gt 0 ? $nsg.Subnets.Id.Split("/")[-1] : @()
-                interfaces = $nsg.NetworkInterfaces.Count -gt 0 ? $nsg.NetworkInterfaces.Id : @()
+                nsgName          = $nsg.Name
+                rules            = $nsg.DefaultSecurityRules | ? { $_.Direction -eq "Inbound" }
+                subnets          = $nsg.Subnets.Count -gt 0 ? $nsg.Subnets.Id : @()
+                interfaces       = $nsg.NetworkInterfaces.Count -gt 0 ? $nsg.NetworkInterfaces.Id : @()
             }
         }
     }
-    foreach($vnet in $vnets)
-    {
-        $subnetNoNsg = $vnet.Subnets | ?{$_.NetworkSecurityGroup.Id -eq $null}
-        foreach($subnet in $vnet.Subnets)
-        {            
-            if($subnet.NetworkSecurityGroup.Id -eq $null)
-            {
+    foreach ($vnet in $vnets) {
+        $subnetNoNsg = $vnet.Subnets | ? { $_.NetworkSecurityGroup.Id -eq $null }
+        foreach ($subnet in $vnet.Subnets) {            
+            if ($subnet.NetworkSecurityGroup.Id -eq $null) {
                 $vnetSubnetsWithoutNSGS += New-Object -TypeName psobject -Property @{
-                    vnet = $vnet.Name
+                    vnet              = $vnet.Name
                     vnetResourceGroup = $vnet.Id.Split("/")[4]
-                    subnetNoNsg = $subnetNoNsg.Name
+                    subnetNoNsg       = $subnetNoNsg.Name
                 }
             }
             $connectedDevices += New-Object -TypeName psobject -Property @{
-                subnet = $subnet.Id
+                subnet     = $subnet.Id
                 interfaces = $subnet.IpConfigurations.Id
             }
         }
     }
     $report += New-Object -TypeName psobject -Property @{
-        SubscriptionName=$sub.Name
-        SubscriptionId = $sub.Id
+        SubscriptionName      = $sub.Name
+        SubscriptionId        = $sub.Id
         NSGsWithSecurityRules = $nsgInRules
-        NSGsWithDefaultRules = $nsgBase
-        VNetsWithNoNSG = $vnetSubnetsWithoutNSGS
-        ConnectedDevices = $connectedDevices
+        NSGsWithDefaultRules  = $nsgBase
+        VNetsWithNoNSG        = $vnetSubnetsWithoutNSGS
+        ConnectedDevices      = $connectedDevices
     }
 }
 
--- Scripts/NSG-Audit/nsg-report-gen.ps1
+++ Scripts/NSG-Audit/nsg-report-gen.ps1
@@ -4,39 +4,74 @@ param (
 
 $data = ConvertFrom-Json $((Get-Content -Path $path) -join "") -Depth 10
 $interfacesReport = @()
+$subnetsReport = @()
 foreach($d in $data)
 {
     foreach($subnet in $d.ConnectedDevices)
     {
         if($subnet.subnet -notlike "*GatewaySubnet")
         {
+            $subnetNsg = $null
+            $subnetdefaultRules = $true
+            $subnetNsg = $d.NSGsWithDefaultRules | ?{$_.subnets -contains $subnet.subnet}
+            if($subnetNsg -eq $null)
+            {
+                $subnetNsg = $d.NSGsWithSecurityRules | ?{$_.subnets -contains $subnet.subnet}
+                $subnetdefaultRules = $false
+            }
             foreach($interface in $subnet.interfaces)
             {
-                $rule = ""
-                $rule = $d.NSGsWithSecurityRules | ?{$_.interfaces -contains $($interface.Split("/")[0..8] -join "/")}
-                if($rule -eq "")
+                $rule = $null
+                $interfaceDefaultRules = $true
+                $rule = $d.NSGsWithDefaultRules | ?{$_.interfaces -contains $($interface.Split("/")[0..8] -join "/")}
+                if($rule -eq $null)
                 {
-                    $rule = $d.NSGsWithDefaultRules | ?{$_.interfaces -contains $($interface.Split("/")[0..8] -join "/")}
+                    $rule = $d.NSGsWithSecurityRules | ?{$_.interfaces -contains $($interface.Split("/")[0..8] -join "/")}
+                    $interfaceDefaultRules = $false
                 }
                 $interfacesReport += New-Object -TypeName psobject -Property @{
                     SubscriptionName = $d.SubscriptionName
                     SubscriptionId = $d.SubscriptionId
                     Interface = $($interface.Split("/")[0..8] -join "/")
-                    NSGName = $rule.nsgName
-                    NSGRg = $rule.nsgResourceGroup
+                    InterfaceNSG = $rule
+                    InterfaceDefaultRules = $interfaceDefaultRules
                     Subnet = $subnet.subnet
+                    SubnetNSG = $subnetNsg
+                    SubnetDefaultRules = $subnetdefaultRules
                 }
             }
         }        
     }
 }
-
-$interfacesWithNoNsg = $interfacesReport | ?{$_.NSGName -eq $null}
-$noNsgReport = @()
-foreach($interface in $interfacesWithNoNsg)
+$reportHtml = @'
+<html>
+<head>
+<title>NSG Audit</title>
+</head>
+<body>
+<p>Interfaces Protected with Default Rules</p>
+<table>
+<tr><th>Interface</th><th>Subnet</th><th>VNET</th><th>Subscription</th></tr>
+{0}
+</table>
+<p>Interfaces With Custom Rules Configured</p>
+<table>
+{1}
+</table>
+<p>Interfaces With No NSG Associated</p>
+<table>
+{2}
+</table>
+</body>
+</html>
+'@
+$interfacesPortectedWithDefaultRules = $interfacesReport | ?{($_.SubnetDefaultRules -eq $false -and $_.InterfaceDefaultRules -eq $true) -or ($_.SubnetDefaultRules -eq $true -and $_.InterfaceDefaultRules -eq $true) -or ($_.SubnetDefaultRules -eq $true -and $_.InterfaceDefaultRules -eq $false)}
+$interfacesCustomRules = $interfacesReport | ?{($_.SubnetDefaultRules -eq $false -and $_.InterfaceDefaultRules -eq $false -and $_.SubnetNSG -ne $null) -or ($_.SubnetDefaultRules -eq $false -and $_.InterfaceDefaultRules -eq $false -and $_.InterfaceNSG -ne $null)}
+$interfacesOpen = $interfacesReport | ?{$_.SubnetNSG -eq $null -and $_.InterfaceNSG -eq $null}
+$htmlInterfacesPortectedWithDefaultRules = @()
+foreach($interface in $interfacesPortectedWithDefaultRules)
 {
-    $noNsgReport += $interface
+    $htmlInterfacesPortectedWithDefaultRules += "<tr><td>$($interface.Interface.split("/")[-1])</td><td>$($interface.Subnet.split("/")[-1])</td><td>$($interface.Subnet.split("/")[8])</td><td>$($interface.SubscriptionId)</td></tr>"
 }
-
-ConvertTo-Json -InputObject $interfacesReport -Depth 10 > "$($PWD.Path)\interfaceReport.json"
-ConvertTo-Json -InputObject $interfacesWithNoNsg -Depth 10 > "$($PWD.Path)\interfacesWithNoNsg.json"
+$($reportHtml -f $($htmlInterfacesPortectedWithDefaultRules -join "")) | Out-File "$($PWD.Path)\interfaceReport.html"
+#ConvertTo-Json -InputObject $interfacesReport -Depth 10 > "$($PWD.Path)\interfaceReport.json"
