1c01cec55c15604eaf0db62ce28d44a1901766e6
Luke Arp <ruarp@microsoft.com>
2023-02-17 09:57:01 -0500

    Approve Script

--- Standard-Deployments/Private-Synapse/Script/Approve-Private.ps1
+++ Standard-Deployments/Private-Synapse/Script/Approve-Private.ps1
@@ -4,4 +4,7 @@ param(
 $connections = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $resourceId | ?{$_.PrivateLinkServiceConnectionState.Status -eq "Pending"}
 foreach($connection in $connections) {
     Approve-AzPrivateEndpointConnection -ResourceId $connection.Id
-}    
+}
+
+$DeploymentScriptOutputs = @{};
+$DeploymentScriptOutputs['resourceId'] = $resourceId
