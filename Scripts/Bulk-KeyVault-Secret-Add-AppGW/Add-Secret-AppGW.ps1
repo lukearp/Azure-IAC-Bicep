4286e8ce30a7d3592153f4699ced66f8473cf2df
Luke Arp <ruarp@microsoft.com>
2023-10-02 09:25:35 -0400

    changed eval

--- Scripts/Bulk-KeyVault-Secret-Add-AppGW/Add-Secret-AppGW.ps1
+++ Scripts/Bulk-KeyVault-Secret-Add-AppGW/Add-Secret-AppGW.ps1
@@ -4,23 +4,24 @@ $keyVaultSecrets = @(
 )
 $managedIdentity = "<Managed-Identity-ResourceID>"
 $applicationGateways = Search-AzGraph -UseTenantScope -Query "resources | where type contains `"applicationGateways`" | project id"; 
-foreach($applicationGateway in $applicationGateways)
-{
+foreach ($applicationGateway in $applicationGateways) {
     $subscription = $applicationGateway.id.split("/")[2]
     $resourceGroup = $applicationGateway.id.split("/")[4]
     $appGatewayName = $applicationGateway.id.split("/")[8]
 
     Select-AzSubscription -Subscription $subscription
     $appGw = Get-AzApplicationGateway -Name $appGatewayName -ResourceGroupName $resourceGroup
-    if($appGw.Identity -eq $null)
-    {
+    if ($appGw.Identity -eq $null) {
         Set-AzApplicationGatewayIdentity -ApplicationGateway $appGw -UserAssignedIdentityId $managedIdentity
     }
-    foreach($keyVaultSecret in $keyVaultSecrets)
-    {
-        if($appGw.SslCertificates.Name.contains($($keyVaultSecret.Split("/")[4] + "-kv").ToUpper()))
-        {
-            Write-Host "Cert already assigned";
+    foreach ($keyVaultSecret in $keyVaultSecrets) {
+        if ($appGw.SslCertificates.Count -ge 0) {
+            if ($appGw.SslCertificates.Name.ToUpper().contains($($keyVaultSecret.Split("/")[4] + "-kv").ToUpper())) {
+                Write-Host "Cert already assigned";
+            }
+            else {
+                Add-AzApplicationGatewaySslCertificate -ApplicationGateway $appGw -Name $($keyVaultSecret.Split("/")[4] + "-kv").ToUpper() -KeyVaultSecretId $keyVaultSecret
+            }
         }
         else {
             Add-AzApplicationGatewaySslCertificate -ApplicationGateway $appGw -Name $($keyVaultSecret.Split("/")[4] + "-kv").ToUpper() -KeyVaultSecretId $keyVaultSecret
