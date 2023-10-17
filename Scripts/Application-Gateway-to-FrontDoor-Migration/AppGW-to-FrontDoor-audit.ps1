ff8c59336968013d2178a853dcb17f140859ad38
Luke Arp <ruarp@microsoft.com>
2023-10-17 10:53:12 -0400

    support path based rules

--- Scripts/Application-Gateway-to-FrontDoor-Migration/AppGW-to-FrontDoor-audit.ps1
+++ Scripts/Application-Gateway-to-FrontDoor-Migration/AppGW-to-FrontDoor-audit.ps1
@@ -6,7 +6,8 @@ param (
 <#
     TODO: Account for Path Based Redirect Rules:
     UrlPathMaps.DefaultRedirectConfiguration for default redirect rule
-    $appGateway.UrlPathMaps.PathRules.Paths
+    $appGateway.UrlPathMaps.PathRules.DefaultRedirectConfiguration.Id
+    $appGateway.UrlPathMaps.PathRules.Paths    
     $appGateway.UrlPathMaps.PathRules.RedirectConfiguration.Id
 #>
 $appGateway = Get-AzApplicationGateway -Name $appGatewayName -ResourceGroupName $appGatewayRg
@@ -14,20 +15,43 @@ $hostNames = $appGateway.HttpListeners.HostNames
 $hostNames += $appGateway.HttpListeners.HostName
 $hostNames = $hostNames | Select -Unique
 Add-Content -Path $csvPath -Value "AppGateway,Listner,HostName,RedirectTarget"
-$rules = @()
-foreach ($listener in $appGateway.HttpListeners) {    
+foreach ($listener in $appGateway.HttpListeners) {   
+    $listenerHostNames = @() 
     $appGwRoutingRule = $appGateway.RequestRoutingRules | ? { $_.HttpListener.Id.Split("/")[-1] -eq $listener.Name }
-    $redirectRule = $appGateway.RedirectConfigurations | ? { $_.Id -eq $appGwRoutingRule.RedirectConfiguration.Id }
-    $listenerHostNames = $listener.HostName
-    $listenerHostNames += $listener.HostNames
-    $customDomains = @()
-    foreach ($hostName in $listenerHostNames) {
-        if($hostName[0] -eq "*" -and $hostName[1] -ne ".")
-        {
-            Add-Content -Path $csvPath -Value "$($appGateway.Name),$($listener.Name),$($hostName.split("*")[1]),$($redirectRule.TargetUrl)"
-            Add-Content -Path $csvPath -Value "$($appGateway.Name),$($listener.Name),$($hostName.replace("*","www.")),$($redirectRule.TargetUrl)"
+    $redirectRule = $null
+    $redirectRule = $appGateway.RedirectConfigurations | ? { $_.Id -eq $appGwRoutingRule.RedirectConfiguration.Id }    
+    if($listener.HostName -ne $null)
+    {
+        if ($listener.HostName[0] -eq "*" -and $listener.HostName[1] -ne ".") {
+            $listenerHostNames += $($listener.HostName.split("*")[1])
+            $listenerHostNames += $($listener.HostName.replace("*", "www."))
         }
         else {
+            $listenerHostNames += $($listener.HostName)
+        }
+    }    
+    foreach ($name in $listener.HostNames) {
+        if ($name[0] -eq "*" -and $name[1] -ne ".") {
+            $listenerHostNames += $($name.split("*")[1])
+            $listenerHostNames += $($name.replace("*", "www."))
+        }
+        else {
+            $listenerHostNames += $($name)
+        }
+    }
+    foreach ($hostName in $listenerHostNames) {
+        if ($redirectRule -eq $null) {
+            $urlMap = $appGateway.UrlPathMaps | ? { $_.Id -eq $appGwRoutingRule.UrlPathMap.Id }            
+            $defaultRedirect = $appGateway.RedirectConfigurations | ? { $_.Id -eq $urlMap.DefaultRedirectConfiguration.Id }
+            Add-Content -Path $csvPath -Value "$($appGateway.Name),$($listener.Name),$($hostName),$($defaultRedirect.TargetUrl)"
+            foreach ($rule in $urlMap.PathRules) {
+                foreach($path in $rule.Paths)
+                {
+                    Add-Content -Path $csvPath -Value "$($appGateway.Name),$($listener.Name),$($hostName + $path),$(($appGateway.RedirectConfigurations | ? { $_.Id -eq $rule.RedirectConfiguration.Id }).TargetUrl)"
+                }
+            }
+        }
+        else { 
             Add-Content -Path $csvPath -Value "$($appGateway.Name),$($listener.Name),$($hostName),$($redirectRule.TargetUrl)"
         }
     }
