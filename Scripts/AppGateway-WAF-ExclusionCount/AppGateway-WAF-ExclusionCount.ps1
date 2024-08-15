14fa409cb6df61c389f97da00e374dabff59f878
Luke Arp <ruarp@microsoft.com>
2024-08-15 14:51:07 -0400

    better report

--- Scripts/AppGateway-WAF-ExclusionCount/AppGateway-WAF-ExclusionCount.ps1
+++ Scripts/AppGateway-WAF-ExclusionCount/AppGateway-WAF-ExclusionCount.ps1
@@ -6,16 +6,29 @@ $associatedPolicies = @($appGateway.FirewallPolicy.Id)
 $associatedPolicies += @($appGateway.HttpListeners.FirewallPolicy.Id)
 $wafPolicies = Get-AzApplicationGatewayFirewallPolicy
 $wafPoliciesByExclusion = @()
-$exclusionCount = 0
 foreach($policy in $wafPolicies)
 {
-    $wafPoliciesByExclusion += New-Object -TypeName psobject -Property @{
-        id = $policy.Id
-        count = $policy.ManagedRules.Exclusions.Count
-    }
+    $countOfExcludedRules = $policy.ManagedRules.Exclusions.ExclusionManagedRuleSets.RuleGroups.Rules.RuleId
+    if($null -ne $countOfExcludedRules)
+    {
+        $wafPoliciesByExclusion += New-Object -TypeName psobject -Property @{
+            id = $policy.Id
+            count = $policy.ManagedRules.Exclusions.Count
+            excludedRules = $countOfExcludedRules | Group-Object | Select-Object Name, Count
+        }
+    }    
+}
+$exclusions = New-Object -TypeName psobject -Property @{
+    totalExclusions = 0 
+    excludedRules = @()
 }
 foreach($associatedPolicy in $associatedPolicies)
-{
-    $exclusionCount += ($wafPoliciesByExclusion | ?{$_.id -eq $associatedPolicy}).count
+{    
+    $exclusions.totalExclusions += ($wafPoliciesByExclusion | ?{$_.id -eq $associatedPolicy}).count
+    if($null -ne ($wafPoliciesByExclusion | ?{$_.id -eq $associatedPolicy}).excludedRules)
+    {
+        $exclusions.excludedRules += ($wafPoliciesByExclusion | ?{$_.id -eq $associatedPolicy}).excludedRules
+    }
+    $exclusionReport += $exclusions
 }
-$exclusionCount
+$exclusions | ConvertTo-Json -Depth 10
