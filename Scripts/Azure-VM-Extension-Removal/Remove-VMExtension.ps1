899275f7b1738a022e27f7d964baa0005e09b8e6
Luke Arp <ruarp@microsoft.com>
2024-09-03 10:56:51 -0400

    remove VM Extension

---
+++ Scripts/Azure-VM-Extension-Removal/Remove-VMExtension.ps1
@@ -0,0 +1,21 @@
+$subscriptionId = ""
+$extensionName = "" #Example: MDE.Windows
+
+$query = @'
+resources
+| where type == "microsoft.hybridcompute/machines"
+| where subscriptionId == "{0}"
+| project name, resourceGroup
+'@
+$servers = Search-AzGraph -Query $($query -f $subscriptionId)
+Select-AzSubscription -Subscription $subscriptionId
+
+foreach($server in $servers)
+{
+    $arcVMExtension = Get-AzConnectedMachineExtension -MachineName $server.name -ResourceGroupName $server.resourceGroup -SubscriptionId $subscriptionId -Name $extensionName
+    if($null -eq $arcVMExtension)
+    {
+        # Remove the extension
+        Remove-AzConnectedMachineExtension -ResourceGroupName $server.resourceGroup -MachineName $server.name -Name $extensionName
+    }
+}
